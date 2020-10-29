bd97774ad58405b154e2ac70ab53c990a5c71dc5
zab3 <xelvis03@gmail.com>
2020-10-29 22:43:07 +0900

    プロフィール画面の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   mount_uploader :img_name, ImgNameUploader
+
+  belongs_to_active_hash :team
 end
