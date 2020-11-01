76c30c47902994de4f7591b9b60864977cabc5db
zab3 <xelvis03@gmail.com>
2020-11-01 15:17:05 +0900

    バリデーション作成

--- app/models/chat_message.rb
+++ app/models/chat_message.rb
@@ -1,4 +1,7 @@
 class ChatMessage < ApplicationRecord
   belongs_to :chat_room
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 500 }
 end
